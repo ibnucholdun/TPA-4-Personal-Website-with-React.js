704f1d2608d5bf84016b4d5791e5c9777d9ceb28
Ibnu Choldun <Ibnucholdun023@gmail.com>
2022-11-03 12:03:13 +0700

    Create portfolio page

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import dataPortfolio from '../../data/dataPortfolio'
+import CardPortfolio from '../../components/Card Portfolio/CardPortfolio.jsx'
+import './Portfolio.css'
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <>
+      <section className="portfolio">
+        <article className="container">
+          <h1><span>-</span> Portfolio <span>-</span></h1>
+          <div className="container-card">
+            {
+              dataPortfolio.map((item, index) => {
+                  return (
+                    <div key={item.id}>
+                      <CardPortfolio img={item.img} link={item.link} name={item.name}/>
+                    </div>
+                  )
+              })
+            }
+          </div>
+        </article>
+      </section>
+    </>
   )
 }
 
