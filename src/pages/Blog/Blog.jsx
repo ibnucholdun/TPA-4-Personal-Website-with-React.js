aeb1dab5ae4abf37eb1af0cbfb95c2ffaba50aef
Ibnu Choldun <Ibnucholdun023@gmail.com>
2022-11-03 12:35:29 +0700

    Create blog page

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import dataBlog from '../../data/dataBlog'
+import CardBlog from '../../components/Card Blog/CardBlog.jsx'
+import './Blog.css'
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <>
+      <section className="blog">
+        <article className="container">
+           <h1><span>-</span> Blog <span>-</span></h1>
+          <div className="container-card">
+            {
+              dataBlog.map((item) => {
+                  return (
+                    <div key={item.id}>
+                      <CardBlog img={item.img} title={item.title} descript={item.descript}/>
+                    </div>
+                  )
+              })
+            }
+          </div>
+        </article>
+      </section>
+    </>
   )
 }
 
